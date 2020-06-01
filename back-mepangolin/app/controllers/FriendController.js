c7507449d4bb165d5a8c32519dce711ea846dd8d
Anthony <anthony.quere@lacatholille.fr>
2020-06-01 16:05:45 +0200

    add FriendController.js

--- back-mepangolin/app/controllers/FriendController.js
+++ back-mepangolin/app/controllers/FriendController.js
@@ -1,16 +1,65 @@
-
+import Pangolin from "../schemas/Pangolin.js";
 class FriendController {
 
-    getFriends (request, response) {
-        console.warn("To implement");
+    getFriends (req, res) {
+        // Get current pangolin
+        Pangolin.findOne({_id: req.userId})
+            .then(currentPangolin => {
+                Pangolin.find({_id: {$in: currentPangolin.friends}})
+                    .then(pangolins => {
+                        res.json(pangolins);
+                    });
+            });
+        return res;
     }
 
-    addFriend (request, response) {
-        console.warn("To Implement");
+    addFriend (req, res) {
+        // Verify that the new friend is not the current pangolin
+        if (req.userId === req.body.id) {
+            res.status(403).json({error: "Vous ne pouvez hélas pas être ami avec vous même"});
+        }
+
+        // Get current pangolin
+        Pangolin.findOne({_id: req.userId})
+            .then(currentPangolin => {
+                // verify that the new friend isn't already a friend
+                if (currentPangolin.friends.indexOf(res.body.id) !== -1) {
+                    res.status(401).json("Vous êtes déjà ami !")
+                    return ;
+                }
+
+                // verify that the other pangolin exists
+                Pangolin.findOne({_id: res.body.id})
+                    .then(friend => {
+                        if (!friend) {
+                            res.status(403).json({"error": "Ce pangolin semble ne pas exister"});
+                            return ;
+                        }
+                        currentPangolin.friends.push(friend._id);
+                        currentPangolin.save((err, pangolin) => {
+                            if (err) {
+                                res.status(500).json("Il semblerait que votre amoitiée soit impossible");
+                                return ;
+                            }
+                            res.json({success: friend.name + " et vous êtes maintenant ami !"});
+                        });
+                    })
+
+            });
+        return res;
     }
 
-    removeFriend (request, response) {
-        console.warn("To Implement");
+    removeFriend (req, res) {
+        Pangolin.findOne({_id: req.userId})
+            .then(currentPangolin => {
+                // verify that the new friend isn't already a friend
+                const oldFriendIndex = currentPangolin.friends.indexOf(res.body.id);
+                if (oldFriendIndex === -1) {
+                    res.status(401).json("Ce pangolin n'était pas votre ami !")
+                    return ;
+                }
+                currentPangolin.friends.splice(oldFriendIndex, 1);
+            });
     }
 
 }
